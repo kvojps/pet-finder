6ac6f76a8f8dd7c02ac7014705e012d46c081bd3
kvojps <joseferreira.santosjunior@upe.br>
2022-04-22 14:59:16 -0300

    adicionando .gitignore

--- src/componentes/paginas/Login.js
+++ src/componentes/paginas/Login.js
@@ -1,6 +1,16 @@
+import styles from './assets/Login.module.css'
+
 function Login() {
     return (
-        <h1>Login</h1>
+        <div>
+            <h1>Faça login no Pet Finder</h1>
+            <div>
+                <form action="" className={styles.form}>
+                    <input type="email" name="" id="" placeholder="Digite seu e-mail"/>
+                    <input type="password" name="" id="" placeholder="Digite sua senha"/>
+                </form>
+            </div>
+        </div>
     )
 }
 
