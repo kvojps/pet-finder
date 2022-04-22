d5bc78b31414b3c4462b4b2883e1ece80926627a
kvojps <joseferreira.santosjunior@upe.br>
2022-04-22 15:23:44 -0300

    finalizando pagina de login

--- src/componentes/paginas/Login.js
+++ src/componentes/paginas/Login.js
@@ -1,14 +1,19 @@
 import styles from './assets/Login.module.css'
+import {Link} from 'react-router-dom'
 
 function Login() {
     return (
-        <div>
-            <h1>Faça login no Pet Finder</h1>
+        <div className={styles.main}>
             <div>
+                <h1>Faça login no Pet Finder</h1>
                 <form action="" className={styles.form}>
                     <input type="email" name="" id="" placeholder="Digite seu e-mail"/>
                     <input type="password" name="" id="" placeholder="Digite sua senha"/>
                 </form>
+                <div>
+                    <button type="submit" className={styles.botao_enviar}>Enviar</button>
+                    <Link to ='/' className={styles.link}> Ainda não é cadastrado? Cadastre-se</Link>
+                </div>
             </div>
         </div>
     )
