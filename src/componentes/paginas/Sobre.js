3844d54adc466d517f758a810ac053c76e270754
kvojps <joseferreira.santosjunior@upe.br>
2022-04-23 09:53:47 -0300

    adicionando pagina de sobre

--- src/componentes/paginas/Sobre.js
+++ src/componentes/paginas/Sobre.js
@@ -1,7 +1,43 @@
+import img_jose from '../../img/integrante-jose.jpg'
+import img_marx from '../../img/integrante-marx.jpeg'
+import img_leo from '../../img/integrante-leo.jpg'
+import img_ivaldir from '../../img/integrante-ivaldir.png'
+
+import styles from './assets/Sobre.module.css'
+
 function Sobre() {
     return (
         <div>
-            <h1>Sobre</h1>
+            <h1>Sobre nós</h1>
+            <p>
+                O Pet Finder surgiu inicialmente em uma disciplina de DCEX4 do curso de Engenharia de
+                software da Universidade de Pernambuco. Temos o intuito de auxiliar pessoas a encontrarem
+                os seus animais desaparecidos, como forma de motivação, a pessoa que perdeu seu pet pode
+                definir uma recompensa para outros que o encontrar.
+            </p>
+            <h1>Integrantes</h1>
+            <section className={styles.integrantes}>
+                <div className={styles.card_integrante}>
+                    <img src={img_jose} alt="" width="200" height="200"/>
+                    <p>José Ferreira</p>
+                    <p>Desenvolvedor</p>
+                </div>
+                <div className={styles.card_integrante}>
+                    <img src={img_marx} alt="" width="200" height="200"/>
+                    <p>Marx Santos</p>
+                    <p>Desenvolvedor</p>
+                </div>
+                <div className={styles.card_integrante}>
+                    <img src={img_leo} alt="" width="200" height="200"/>
+                    <p>Leonardo Vitor</p>
+                    <p>Desenvolvedor</p>
+                </div>
+                <div className={styles.card_integrante}>
+                    <img src={img_ivaldir} alt="" width="200" height="200"/>
+                    <p>Ivaldir Júnior</p>
+                    <p>Orientador</p>
+                </div>
+            </section>
         </div>
     )
 }
